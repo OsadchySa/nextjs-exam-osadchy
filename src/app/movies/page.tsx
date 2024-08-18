1bfaad84204d4d5ab0bc3323ae42bf671cecf3e5
OsadchySa <osadchyalexandr0790@gmail.com>
2024-08-18 12:28:00 +0300

    18.08

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -8,18 +8,30 @@ interface MoviesPageProps {
     searchParams: { page?: string }
 }
 
-const MoviesPage = async ({ searchParams }: MoviesPageProps) => {
+interface Movie {
+    id: number;
+    title: string;
+    poster_path: string;
+    vote_average: number
+}
 
-    const currentPage = searchParams?.page ? parseInt(searchParams.page, 10) : 1
-    const movies = await getMoviesByNumOfPage(currentPage)
+interface MoviesResponse {
+    results: Movie[];
+    total_pages: number
+}
+
+const MoviesPage = async ({ searchParams }: MoviesPageProps) => {
+    const currentPage = searchParams?.page ? parseInt(searchParams.page, 10) : 1;
+    const movies: any = await getMoviesByNumOfPage(currentPage);
+
     return (
         <div>
             <div className="moviesPage">
-                {movies.results.map((movie) => (
+                {movies.results.map((movie: Movie) => (
                     <div className="littleCardMovie" key={movie.id}>
                         <Link href={{
-                            pathname:'/movies/'+movie.title,
-                            query:{data:JSON.stringify(movie)}
+                            pathname: '/movies/' + movie.title,
+                            query: { data: JSON.stringify(movie) }
                         }}>
                             <img
                                 className="allOfImg"
@@ -28,21 +40,20 @@ const MoviesPage = async ({ searchParams }: MoviesPageProps) => {
                             />
                         </Link>
                         <Link href={{
-                            pathname:'/movies/'+movie.title,
-                            query:{data:JSON.stringify(movie)}
+                            pathname: '/movies/' + movie.title,
+                            query: { data: JSON.stringify(movie) }
                         }}>
                             <h3>{movie.title}</h3>
                         </Link>
-                        <StarRatingComponent rating={movie.vote_average}/>
+                        <StarRatingComponent rating={movie.vote_average} />
                     </div>
                 ))}
             </div>
 
-
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
                 <Link href={`?page=${currentPage - 1}`}>
                     <button className={'pagBut'}
-                        disabled={currentPage === 1}
+                            disabled={currentPage === 1}
                     >
                         Previous
                     </button>
@@ -52,7 +63,7 @@ const MoviesPage = async ({ searchParams }: MoviesPageProps) => {
                 </div>
                 <Link href={`?page=${currentPage + 1}`}>
                     <button className={'pagBut'}
-                        disabled={currentPage === movies.total_pages}
+                            disabled={currentPage === movies.total_pages}
                     >
                         Next
                     </button>
